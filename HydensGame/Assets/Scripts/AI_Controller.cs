173d8d1a934c4896921cdb66dbf8597a88a58e0e
HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>
2022-03-23 02:35:58 +0000

    Fixed AI after making attack state Signed-off-by: HydenJohnson75 <78604065+ToxicOddesey@users.noreply.github.com>

--- HydensGame/Assets/Scripts/AI_Controller.cs
+++ HydensGame/Assets/Scripts/AI_Controller.cs
@@ -7,14 +7,16 @@ using UnityEngine.AI;
 public class AI_Controller : MonoBehaviour,I_Shootable
 {
     Manager my_Manager;
-    enum ai_State { Alive, Dying, Dead, Attacking, Searching, Idle, StartingPatrol}
+    enum ai_State { Alive, Dying, Dead, Following, Searching, Idle, StartingPatrol, Attacking}
     internal int spawn_point_index = 0;
     int enemy_HP;
+    [SerializeField]
     ai_State current_State;
     Animator ai_Animation;
 
     private Transform target;
-    public float attack_Radius = 30f;
+    float follow_Radius = 20f;
+    public float attack_Radius = 10f;
     private NavMeshAgent navMesh;
     public Transform[] patrolPoints;
     float death_Timer = 3f;
@@ -42,11 +44,11 @@ public class AI_Controller : MonoBehaviour,I_Shootable
     {
         float distanceTo = Vector3.Distance(transform.position, target.position);
 
-        if(distanceTo <= attack_Radius && current_State != ai_State.Dead)
+        if(distanceTo <= follow_Radius && distanceTo >= attack_Radius && current_State != ai_State.Dead)
         {
             navMesh.isStopped = false;
 
-            current_State = ai_State.Attacking;
+            current_State = ai_State.Following;
 
 
             transform.LookAt(target);
@@ -57,6 +59,11 @@ public class AI_Controller : MonoBehaviour,I_Shootable
                 
         }
 
+        if(distanceTo <= attack_Radius && current_State != ai_State.Dead)
+        {
+            current_State = ai_State.Attacking;
+        }
+
         if (current_State == ai_State.Dead)
         {
             navMesh.velocity = Vector3.zero;
@@ -64,15 +71,12 @@ public class AI_Controller : MonoBehaviour,I_Shootable
             my_Manager.Im_Dead(this);
         }
 
-        if (distanceTo > attack_Radius && current_State != ai_State.StartingPatrol)
+        if (distanceTo > follow_Radius && current_State != ai_State.StartingPatrol && current_State != ai_State.Attacking)
         {
-            navMesh.velocity = Vector3.zero;
 
             current_State = ai_State.Searching;
         }
 
-
-        print(enemy_HP);
 
         if(enemy_HP <= 0)
         {
@@ -95,7 +99,7 @@ public class AI_Controller : MonoBehaviour,I_Shootable
 
                 break;
 
-            case ai_State.Attacking:
+            case ai_State.Following:
 
                 followPlayer();
 
@@ -119,7 +123,15 @@ public class AI_Controller : MonoBehaviour,I_Shootable
 
                 break;
 
+            case ai_State.Attacking:
+
+                attackPlayer();
+
+                break;
+
         }
+
+        
     }
 
     private void ai_Idle()
@@ -127,6 +139,7 @@ public class AI_Controller : MonoBehaviour,I_Shootable
         ai_Animation.SetBool("is_Idle", true);
         ai_Animation.SetBool("is_Running", false);
         ai_Animation.SetBool("is_Dead", false);
+        ai_Animation.SetBool("is_Attacking", false);
     }
 
     internal void addManager(Manager manager, int spawn_index)
@@ -145,6 +158,7 @@ public class AI_Controller : MonoBehaviour,I_Shootable
         ai_Animation.SetBool("is_Idle", false);
         ai_Animation.SetBool("is_Running", false);
         ai_Animation.SetBool("is_Dead", true);
+        ai_Animation.SetBool("is_Attacking", false);
     }
 
     internal void followPlayer()
@@ -152,6 +166,8 @@ public class AI_Controller : MonoBehaviour,I_Shootable
         ai_Animation.SetBool("is_Idle", false);
         ai_Animation.SetBool("is_Running", true);
         ai_Animation.SetBool("is_Dead", false);
+        ai_Animation.SetBool("is_Attacking", false);
+        navMesh.isStopped = false;
     }
 
     internal void startPatrol()
@@ -159,6 +175,7 @@ public class AI_Controller : MonoBehaviour,I_Shootable
         ai_Animation.SetBool("is_Idle", false);
         ai_Animation.SetBool("is_Running", true);
         ai_Animation.SetBool("is_Dead", false);
+        ai_Animation.SetBool("is_Attacking", false);
 
         transform.position = Vector3.MoveTowards(transform.position, startPoint.position, 3 * Time.deltaTime);
 
@@ -174,16 +191,29 @@ public class AI_Controller : MonoBehaviour,I_Shootable
 
     internal void randomPatrol()
     {
+        navMesh.enabled = false;
+        navMesh.enabled = true;
+
         transform.position = Vector3.MoveTowards(transform.position, movePoints[randomPoint].position, 3 * Time.deltaTime);
 
         transform.LookAt(movePoints[randomPoint]);
 
-        if (Vector3.Distance(transform.position, movePoints[randomPoint].position) < 1f)
+        if (Vector3.Distance(transform.position, movePoints[randomPoint].position) <= 2f)
         {
-                randomPoint = UnityEngine.Random.Range(0, movePoints.Length);
-
+            randomPoint = UnityEngine.Random.Range(0, movePoints.Length);
+            
         }
     }
 
+    internal void attackPlayer()
+    {
+        transform.LookAt(target);
+        ai_Animation.SetBool("is_Idle", false);
+        ai_Animation.SetBool("is_Running", false);
+        ai_Animation.SetBool("is_Dead", false);
+        ai_Animation.SetBool("is_Attacking", true);
+        navMesh.isStopped = true;
+
+    }
 
 }
